dc8cab41622c8b73110b8258aa4e28679ce0fc92
oshadakavinda <oshadakavinda2@gmail.com>
2024-08-11 23:37:39 +0530

    Changes to be committed: modified:   .env 	modified:   pages/api/checkout.js 	new file:   pages/api/webhook.js 	modified:   pages/cart.js

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -23,14 +23,15 @@ export default async function handler(req,res) {
     const productInfo = productsInfos.find(p => p._id.toString() === productId);
     const quantity = productsIds.filter(id => id === productId)?.length || 0;
     if (quantity > 0 && productInfo) {
-      line_items.push({
-        quantity,
-        price_data: {
-          currency: 'USD',
-          product_data: {name:productInfo.title},
-          unit_amount: quantity * productInfo.price * 100,
-        },
-      });
+        line_items.push({
+            quantity,
+            price_data: {
+              currency: 'LKR',
+              product_data: { name: productInfo.title },
+              unit_amount: productInfo.price * 100, // Corrected: price per single unit
+            },
+          });
+          
     }
   }
 
