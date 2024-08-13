2ecf657cbb75658a628b40867f6cb26e156bed84
oshadakavinda <oshadakavinda2@gmail.com>
2024-08-13 13:19:24 +0530

    Changes to be committed: 	modified:   .env     modified:   package-lock.json 	modified:   package.json 	modified:   pages/api/webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,1 +1,45 @@
-export {mongooseConnect} 
+import {mongooseConnect} from "@/lib/mongoose";
+const stripe = require('stripe')(process.env.STRIPE_SK);
+import {buffer} from 'micro';
+import {Order} from "@/models/Order";
+
+const endpointSecret = "whsec_e9435f2684d460d61f3f751de64f042758266bda2ef4d904f2ba95c8bea8772c";
+
+export default async function handler(req,res) {
+  await mongooseConnect();
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === 'paid';
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId,{
+          paid:true,
+        })
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send('ok');
+}
+
+export const config = {
+  api: {bodyParser:false,}
+};
+
+// bright-thrift-cajole-lean
+// acct_1PlnPdC53g1V3AxY
