e52273db6f364790716ac2569003d4d2c63f1e43
oshadakavinda <oshadakavinda2@gmail.com>
2024-08-15 17:11:08 +0530

    Changes to be committed: 	new file:   components/ProductImages.js 	new file:   components/WhiteBox.js 	modified:   pages/product/[id].js

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -3,37 +3,91 @@ import Title from "@/components/Title";
 import Header from "@/components/Header";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+import WhiteBox from "@/components/WhiteBox";
+import styled from "styled-components";
+import ProductImages from "@/components/ProductImages";
+import { CartContext } from "@/components/CartContext";
+import { useContext } from "react";
+import Button from "@/components/Button";
+import CartIcon from "@/components/icons/CartIcon";
+
+const ColWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 0.8fr 1.2fr;
+  }
+  gap: 40px;
+  margin: 40px 0;
+`;
+
+const PriceRow = styled.div`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+`;
+
+const Price = styled.span`
+  font-size: 1.4rem;
+`;
 
 export default function ProductPage({ product }) {
+  const { addProduct } = useContext(CartContext);
+
+  if (!product) {
+    return (
+      <Center>
+        <Title>Product Not Found</Title>
+      </Center>
+    );
+  }
+
   return (
     <>
-      <Header>
-        <Center>
-          <Title>{product?.title || "Product not found"}</Title>
-        </Center>
-      </Header>
+      <Header />
+      <Center>
+        <ColWrapper>
+          <WhiteBox>
+            <ProductImages images={product.images || []} />
+          </WhiteBox>
+          <div>
+            <Title>{product.title || "No Title Available"}</Title>
+            <p>{product.description || "No description available"}</p>
+            <PriceRow>
+              <div>
+                <Price>Rs. {product.price}</Price>
+              </div>
+              <div>
+                <Button primary onClick={() => addProduct?.(product._id)}>
+                  <CartIcon /> Add to cart
+                </Button>
+              </div>
+            </PriceRow>
+          </div>
+        </ColWrapper>
+      </Center>
     </>
   );
 }
 
 export async function getServerSideProps(context) {
-    await mongooseConnect();
+  await mongooseConnect();
+
+  const { id } = context.query;
   
-    const { id } = context.query;
-    const product = await Product.findById(id).lean();
-  
-    //console.log(context.query); // Log the product data
-    //console.log(product);
-    if (!product) {
-      return {
-        notFound: true,
-      };
-    }
-  
+  // Fetch the product and convert it to a plain JS object
+  const product = await Product.findById(id).lean();
+
+  // Handle the case where the product is not found
+  if (!product) {
     return {
-      props: {
-        product: JSON.parse(JSON.stringify(product)),
-      },
+      notFound: true,
     };
   }
-  
+
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product)),
+    },
+  };
+}
