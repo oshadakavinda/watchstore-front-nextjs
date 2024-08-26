1486db532a401253ef2ec2de09349fcb44b3a852
oshadakavinda <oshadakavinda2@gmail.com>
2024-08-26 23:35:19 +0530

    new file:   pages/account.js

---
+++ pages/account.js
@@ -0,0 +1,17 @@
+import Header from "@/components/Header"
+import Center from "@/components/Center"
+import Title from "@/components/Title"
+
+
+export default function AccountPage(){
+    return(
+        <>
+        <Header>
+        <Center>
+            <Title>Account</Title>
+        </Center>
+
+        </Header>
+        </>
+    )
+}
